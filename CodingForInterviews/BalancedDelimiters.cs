e1528bbaaf6919cab189517057b6184ac989eccd
bdrupieski <brian.drupieski@gmail.com>
2014-12-07 22:58:16 -0500

    balanced delimiter

---
+++ CodingForInterviews/BalancedDelimiters.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CodingForInterviews
+{
+    public class BalancedDelimiters
+    {
+        public static List<Delimiter> Delimiters = new List<Delimiter>
+        {
+            new Delimiter('{', '}'),
+            new Delimiter('(', ')'),
+            new Delimiter('[', ']'),
+        };
+
+        public static Dictionary<char, Delimiter> DelimiterByOpeningCharacter = Delimiters.ToDictionary(x => x.Open);
+        public static HashSet<char> OpeningDelimiters = Delimiters.Select(x => x.Open).ToHashSet();
+        public static HashSet<char> ClosingDelimiters = Delimiters.Select(x => x.Close).ToHashSet();
+
+        public static bool IsBalanced(string s)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var character in s)
+            {
+                if (stack.IsEmpty() && IsClosingDelimiter(character))
+                {
+                    return false;
+                }
+                else if (IsOpeningDelimiter(character))
+                {
+                    stack.Push(character);
+                }
+                else if (IsClosingDelimiter(character))
+                {
+                    char openingDelimiterCharacter = stack.Pop();
+                    Delimiter d = DelimiterByOpeningCharacter[openingDelimiterCharacter];
+
+                    // the current closing delimiter is not the closing delimiter for the last opening delimiter
+                    if (d.Close != character)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // anything left over? then something wasn't closed
+            if (stack.Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsOpeningDelimiter(char s)
+        {
+            return OpeningDelimiters.Contains(s);
+        }
+
+        private static bool IsClosingDelimiter(char s)
+        {
+            return ClosingDelimiters.Contains(s);
+        }
+
+        public class Delimiter
+        {
+            public Delimiter(char open, char close)
+            {
+                Open = open;
+                Close = close;
+            }
+
+            public char Open { get; private set; }
+            public char Close { get; private set; }
+        }
+
+        [TestFixture]
+        public class Tests
+        {
+            [Test]
+            public void TestIsBalanced()
+            {
+                Assert.True(IsBalanced("()"));
+                Assert.True(IsBalanced("()()()"));
+                Assert.True(IsBalanced("()[]{}"));
+                Assert.True(IsBalanced("([{}])"));
+                Assert.True(IsBalanced("([]{})"));
+                Assert.True(IsBalanced("[[[[][]][][[][]][][][[][[]]]]]"));
+
+                Assert.False(IsBalanced(")"));
+                Assert.False(IsBalanced("("));
+                Assert.False(IsBalanced("([)]"));
+                Assert.False(IsBalanced("([]"));
+                Assert.False(IsBalanced("[])"));
+                Assert.False(IsBalanced("([})"));
+                Assert.False(IsBalanced("{{{{}}}"));
+                Assert.False(IsBalanced("[][][]]"));
+                Assert.False(IsBalanced("[[][][]"));
+            }
+        }
+    }
+}
