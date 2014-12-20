8790bb0a7d1f094119a416b8f46a740bd84719b4
bdrupieski <brian.drupieski@gmail.com>
2014-12-20 14:07:24 -0500

    kth to last element of linked list

---
+++ CrackingTheCodingInterview/Chapter2/Q2P2FindKthToLastElementOfLinkedList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace CrackingTheCodingInterview.Chapter2
+{
+    /// <summary>
+    /// Implement an algorithm to find the kth to last element of a singly linked list
+    /// </summary>
+    public class Q2P2FindKthToLastElementOfLinkedList
+    {
+        public static Node<T> FindKthToLastElement_Iterative<T>(Node<T> head, int k) where T : IEquatable<T>
+        {
+            Node<T> last = head;
+            Node<T> kthToLast = head;
+
+            for (int i = 0; i < k; i++)
+            {
+                if (last.Next == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    last = last.Next;
+                }
+            }
+
+            while (last.Next != null)
+            {
+                last = last.Next;
+                kthToLast = kthToLast.Next;
+            }
+
+
+            return kthToLast;
+        }
+
+        public static Node<T> FindKthToLastElement_Recursive<T>(Node<T> head, int k) where T : IEquatable<T>
+        {
+            int currentElement = 0;
+            return FindKthToLastElement_Recursive_Impl(head, k, ref currentElement);
+        }
+
+        private static Node<T> FindKthToLastElement_Recursive_Impl<T>(Node<T> head, int k, ref int currentElement) where T : IEquatable<T>
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            var node = FindKthToLastElement_Recursive_Impl(head.Next, k, ref currentElement);
+            currentElement++;
+            if (currentElement == k + 1)
+            {
+                return head;
+            }
+            return node;
+        }
+
+        [TestFixture]
+        public class Q2P2FindKthToLastElementOfLinkedListTests
+        {
+            [Test]
+            public void FindKthToLastElementsTest()
+            {
+                foreach (var m in typeof(Q2P2FindKthToLastElementOfLinkedList).PublicStaticMethods())
+                {
+                    var head = new Node<int>(1);
+                    head.Append(2);
+                    head.Append(3);
+                    head.Append(4);
+                    head.Append(5);
+
+                    Assert.AreEqual(5, m.FindKthToLastElement(head, 0).Data);
+                    Assert.AreEqual(4, m.FindKthToLastElement(head, 1).Data);
+                    Assert.AreEqual(3, m.FindKthToLastElement(head, 2).Data);
+                    Assert.AreEqual(2, m.FindKthToLastElement(head, 3).Data);
+                    Assert.AreEqual(1, m.FindKthToLastElement(head, 4).Data);
+
+                    Assert.AreEqual(null, m.FindKthToLastElement(head, 5));
+                    Assert.AreEqual(null, m.FindKthToLastElement(head, 6));
+                }
+            }
+
+            [Test]
+            [Ignore]
+            public void TestQ1P5RunLengthEncodingPerformance()
+            {
+                var head = new Node<int>(1);
+                head.Append(2);
+                head.Append(3);
+                head.Append(4);
+                head.Append(5);
+
+                PerformanceHelper.PerformanceTestPublicStaticMethods<Q2P2FindKthToLastElementOfLinkedList, int>(head, 4);
+            }
+        }
+    }
+
+    public static class Q2P2FindKthToLastElementOfLinkedListMethodExtensions
+    {
+        public static Node<T> FindKthToLastElement<T>(this MethodInfo m, Node<T> head, int k) where T : IEquatable<T>
+        {
+            return (Node<T>)m.MakeGenericMethod(typeof(T)).Invoke(null, new object[] { head, k });
+        }
+    }
+}
--- CrackingTheCodingInterview/PerformanceHelper.cs
+++ CrackingTheCodingInterview/PerformanceHelper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 
 namespace CrackingTheCodingInterview
 {
@@ -7,7 +10,19 @@ namespace CrackingTheCodingInterview
     {
         public static void PerformanceTestPublicStaticMethods<T>(params object[] methodArgs)
         {
-            foreach (var m in typeof(T).PublicStaticMethods())
+            var methods = typeof(T).PublicStaticMethods();
+            PerformanceTest(methods, methodArgs);
+        }
+
+        public static void PerformanceTestPublicStaticMethods<T, T2>(params object[] methodArgs)
+        {
+            var methods = typeof(T).PublicStaticMethods().Select(x => x.MakeGenericMethod((typeof(T2))));
+            PerformanceTest(methods, methodArgs);
+        }
+
+        private static void PerformanceTest(IEnumerable<MethodInfo> methods, object[] methodArgs)
+        {
+            foreach (var m in methods)
             {
                 var sw = Stopwatch.StartNew();
                 for (int i = 0; i < 1000000; i++)
