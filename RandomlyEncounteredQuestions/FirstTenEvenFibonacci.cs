6f3f651961c34d8f1ead2c3369570e98c89fa448
bdrupieski <brian.drupieski@gmail.com>
2015-08-31 20:54:01 -0400

    First ten even Fibonacci numbers

---
+++ RandomlyEncounteredQuestions/FirstTenEvenFibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace RandomlyEncounteredQuestions
+{
+    public class FirstTenEvenFibonacci
+    {
+        static IEnumerable<int> Fibonacci()
+        {
+            int a = 0;
+            int b = 1;
+            while (true)
+            {
+                yield return a;
+                var temp = b;
+                b = a + b;
+                a = temp;
+            }
+        }
+
+        [Test]
+        public void FirstTenEvenFibonacciTest()
+        {
+            CollectionAssert.AreEqual(Fibonacci().Where(x => x % 2 == 0).Take(10), new[] { 0, 2, 8, 34, 144, 610, 2584, 10946, 46368, 196418 });
+        }
+    }
+}
