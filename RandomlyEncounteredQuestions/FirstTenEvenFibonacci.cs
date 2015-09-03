bc27e539351c8c2cd463549a9ea455de2e5568f7
bdrupieski <brian.drupieski@gmail.com>
2015-09-02 20:51:17 -0400

    Recursive Fibonacci

--- RandomlyEncounteredQuestions/FirstTenEvenFibonacci.cs
+++ RandomlyEncounteredQuestions/FirstTenEvenFibonacci.cs
@@ -6,7 +6,7 @@ namespace RandomlyEncounteredQuestions
 {
     public class FirstTenEvenFibonacci
     {
-        static IEnumerable<int> Fibonacci()
+        static IEnumerable<int> FibonacciIterative()
         {
             int a = 0;
             int b = 1;
@@ -19,10 +19,20 @@ namespace RandomlyEncounteredQuestions
             }
         }
 
+        static IEnumerable<int> FibonacciRecursive(int a = 0, int b = 1)
+        {
+            yield return a;
+            foreach (var elem in FibonacciRecursive(b, a + b))
+            {
+                yield return elem;
+            }
+        }
+
         [Test]
         public void FirstTenEvenFibonacciTest()
         {
-            CollectionAssert.AreEqual(Fibonacci().Where(x => x % 2 == 0).Take(10), new[] { 0, 2, 8, 34, 144, 610, 2584, 10946, 46368, 196418 });
+            CollectionAssert.AreEqual(FibonacciIterative().Where(x => x % 2 == 0).Take(10), new[] { 0, 2, 8, 34, 144, 610, 2584, 10946, 46368, 196418 });
+            CollectionAssert.AreEqual(FibonacciRecursive().Where(x => x % 2 == 0).Take(10), new[] { 0, 2, 8, 34, 144, 610, 2584, 10946, 46368, 196418 });
         }
     }
 }
