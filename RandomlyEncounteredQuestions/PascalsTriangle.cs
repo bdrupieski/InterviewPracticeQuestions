63bc62c322b6076c1bc81d4a9f0716e9937ab577
bdrupieski <brian.drupieski@gmail.com>
2015-09-08 20:10:09 -0400

    Pascal's Triangle

---
+++ RandomlyEncounteredQuestions/PascalsTriangle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace RandomlyEncounteredQuestions
+{
+    public class PascalsTriangle
+    {
+        public IEnumerable<IEnumerable<int>> GeneratePascalsTriangle()
+        {
+            yield return new[] { 1 };
+
+            IEnumerable<int> next = new[] { 1, 1 };
+
+            while (true)
+            {
+                yield return next;
+
+                var added = next.Zip(next.Skip(1), (x, y) => x + y);
+
+                next = new[] { 1 }.Concat(added).Concat(new[] { 1 }).ToArray();
+            }
+        }
+
+        [Test]
+        public void PascalsTriangleTest()
+        {
+            var p = GeneratePascalsTriangle().Take(7);
+
+            int[][] t =
+            {
+                new[] { 1 },
+                new[] { 1, 1 },
+                new[] { 1, 2, 1 },
+                new[] { 1, 3, 3, 1 },
+                new[] { 1, 4, 6, 4, 1 },
+                new[] { 1, 5, 10, 10, 5, 1 },
+                new[] { 1, 6, 15, 20, 15, 6, 1 },
+            };
+
+            CollectionAssert.AreEqual(p, t);
+        }
+    }
+}
