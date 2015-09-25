b7240754891274f7cf7de4e77899dfa7ec996b2c
bdrupieski <brian.drupieski@gmail.com>
2015-09-25 01:13:35 -0400

    water filling valleys in an array

---
+++ RandomlyEncounteredQuestions/WaterFillingValleysInAnArray.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RandomlyEncounteredQuestions
+{
+    /// <summary>
+    /// Consider an array. The value at each index of the array represents the height of a wall.
+    /// Now imagine it rains. How much water will accumulate between the walls?
+    /// The volume of the water is measured in 1x1 blocks in the array.
+    /// For example, for the array [1, 0, 4, 3] one block of water accumulates between the 1 and 4,
+    /// but rolls off to the right of the 4 because there is no wall at the end of the array.
+    /// 
+    /// Write a program to compute the volume of water in the puddle.
+    /// </summary>
+    public class WaterFillingValleysInAnArray
+    {
+        private static int GetVolume(int[] arr)
+        {
+            int leftIndex = 0;
+            int rightIndex = arr.Length - 1;
+
+            int leftMaxSoFar = 0;
+            int rightMaxSoFar = 0;
+
+            int volume = 0;
+
+            while (leftIndex < rightIndex)
+            {
+                int leftHeight = arr[leftIndex];
+                int rightHeight = arr[rightIndex];
+
+                int shortestMax = Math.Min(leftMaxSoFar, rightMaxSoFar);
+
+                if (leftHeight < shortestMax)
+                {
+                    volume += shortestMax - leftHeight;
+                    leftIndex++;
+                }
+                else if (rightHeight < shortestMax)
+                {
+                    volume += shortestMax - rightHeight;
+                    rightIndex--;
+                }
+                else
+                {
+                    if (leftHeight < rightHeight)
+                    {
+                        leftIndex++;
+                    }
+                    else
+                    {
+                        rightIndex--;
+                    }
+                }
+
+                leftMaxSoFar = Math.Max(leftMaxSoFar, leftHeight);
+                rightMaxSoFar = Math.Max(rightMaxSoFar, rightHeight);
+            }
+
+            return volume;
+        }
+
+        [Test]
+        public void FillTest()
+        {
+            Assert.AreEqual(17, GetVolume(new[] { 2, 5, 1, 3, 1, 2, 1, 7, 7, 6 }));
+            Assert.AreEqual(0, GetVolume(new[] { 1, 2, 1 }));
+
+            Assert.AreEqual(1, GetVolume(new[] { 2, 1, 2 }));
+            Assert.AreEqual(2, GetVolume(new[] { 5, 4, 4, 5 }));
+            Assert.AreEqual(3, GetVolume(new[] { 1, 0, 0, 0, 1 }));
+
+            Assert.AreEqual(2, GetVolume(new[] { 1, 0, 1, 0, 1 }));
+            Assert.AreEqual(5, GetVolume(new[] { 2, 0, 1, 0, 2 }));
+            Assert.AreEqual(4, GetVolume(new[] { 2, 0, 2, 0, 2 }));
+
+            Assert.AreEqual(0, GetVolume(new[] { 6, 5, 4, 3, 2, 1 }));
+            Assert.AreEqual(0, GetVolume(new[] { 1, 2, 3, 4, 5, 6 }));
+
+            Assert.AreEqual(7, GetVolume(new[] { 7, 4, 3, 7, 6, 5 }));
+            Assert.AreEqual(7, GetVolume(new[] { 5, 6, 7, 3, 4, 7 }));
+
+
+            Assert.AreEqual(0, GetVolume(new[] { 1, 2, 3, 4, 5, 4, 3, 2, 1 }));
+            Assert.AreEqual(0, GetVolume(new[] { 1, 2, 3, 4, 5, 4, 3, 2, 1 }));
+
+            Assert.AreEqual(5, GetVolume(new[] { 3, 5, 2, 7, 8, 4, 6, 4 }));
+            Assert.AreEqual(5, GetVolume(new[] { 4, 6, 4, 8, 7, 2, 5, 3 }));
+
+            Assert.AreEqual(24, GetVolume(new[] { 4, 0, 3, 0, 2, 0, 5, 2, 1, 0, 4 }));
+            Assert.AreEqual(24, GetVolume(new[] { 4, 0, 1, 2, 5, 0, 2, 0, 3, 0, 4 }));
+        }
+    }
+}
